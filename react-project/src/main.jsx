8c6c98f17b4c0b0fc803d7af2b994d6153b90fb5
Richard Pavia <65116711+orachard@users.noreply.github.com>
2026-01-10 13:58:36 +0000

    Destructured an array

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const [first, second, third, fourth] = ["Dog", "Cat", "Elephant", "Giraffe"];
+console.log(fourth);
+
 createRoot(document.getElementById("root")).render(<App />);
