079c23f1f7b16f092e7d99d96bb1269d2361c68a
Richard Pavia <65116711+orachard@users.noreply.github.com>
2026-01-10 07:59:41 +0000

    Added component properties

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
-function Header() {
+function Header(props) {
+  console.log(props);
   return (
     <header>
-      <h1>Eve's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
+      <p>Copyright {props.year}</p>
     </header>
   );
 }
@@ -11,7 +13,7 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header />
+      <Header name="Richard" year={new Date().getFullYear()} />
       <main>
         <h2>We serve the most delicious food around</h2>
       </main>
