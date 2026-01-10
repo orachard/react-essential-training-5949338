d8515fd1b14d125d00c06ed8477c8009038f0141
Richard Pavia <65116711+orachard@users.noreply.github.com>
2026-01-10 12:40:37 +0000

    Added keys to list items

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -11,14 +11,20 @@ function Header({ name, year }) {
 const items = [
   "Macaroni and Cheese",
   "Salmon with Potatoes",
+  "Chicken Curry",
   "Tofu with Vegetables"
 ];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
 
 function Main({ dishes }) {
   return (
     <ul>
       {dishes.map((dish) => (
-        <li style={{ listStyleType: "none" }}>{dish}</li>
+        <li key={dish.id} style={{ listStyleType: "none" }}>{dish.title}</li>
       ))}
     </ul>
   );
@@ -28,7 +34,7 @@ function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <Main dishes={items} />
+      <Main dishes={dishObjects} />
     </div>
   );
 }
