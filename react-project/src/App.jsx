a411b6eb5f2479bb4bac453231ed149189dc3d88
Richard Pavia <65116711+orachard@users.noreply.github.com>
2026-01-11 06:44:15 +0000

    managing state with useReducer

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import "./App.css";
 import chef from "./images/chef.jpg";
 
@@ -26,7 +26,7 @@ function Main({ dishes, openStatus, onStatus }) {
   return (
     <>
       <div>
-        <button onClick={() => onStatus(true)}>
+        <button onClick={onStatus}>
           I want to be open
         </button>
         <h2>
@@ -56,7 +56,7 @@ function Main({ dishes, openStatus, onStatus }) {
 }
 
 function App() {
-  const [status, setStatus] = useState(true);
+  const [status, toggle] = useReducer((status) => !status, true );
 
   return (
     <div>
@@ -64,14 +64,14 @@ function App() {
         The restaurant is currently{" "}
         {status ? "open" : "closed"}.
       </h1>
-      <button onClick={() => setStatus(!status)}>
+      <button onClick={toggle}>
         {status ? "Close" : "Open"} Restaurant
       </button>
       <Header name="Alex" year={new Date().getFullYear()} />
       <Main
         dishes={dishObjects}
         openStatus={status}
-        onStatus={setStatus}
+        onStatus={toggle}
       />
     </div>
   );
